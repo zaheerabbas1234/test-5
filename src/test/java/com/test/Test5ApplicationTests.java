1430d3462f591c5a9f84cbe59be6647133474d54
ambarisha <zaheer.abbas1506@gmail.com>
2024-10-16 14:31:49 +0530

    first commit

---
+++ src/test/java/com/test/Test5ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test5ApplicationTests {
+
+	@Test
+	void additiontest() {
+		assertEquals(10,Calculator.add(5, 5));
+	}
+
+}
